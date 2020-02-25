ff08d7cc8804234fe76d16fd4d233b4d5bb055e2
Qiang Wen <csqiangwen@gmail.com>
2020-02-25 10:39:52 +0800

    Add files via upload

---
+++ options/base_options.py
@@ -0,0 +1,67 @@
+import argparse
+import os
+import torch
+
+
+class BaseOptions():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        self.initialized = False
+
+    def initialize(self):
+        self.parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, etc)')
+        self.parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
+        self.parser.add_argument('--loadSize', type=int, default=256, help='input resolution')
+        self.parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
+        self.parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in first conv layer')
+        self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints_pixelization', help='models are saved here')
+        self.parser.add_argument('--no_dropout', action='store_true', help='no dropout for the generator')
+        self.parser.add_argument('--resize_or_crop', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
+
+        self.initialized = True
+
+    def parse(self):
+        if not self.initialized:
+            self.initialize()
+        self.opt = self.parser.parse_args()
+        self.opt.isTrain = self.isTrain   # train or test
+
+        str_ids = self.opt.gpu_ids.split(',')
+        self.opt.gpu_ids = []
+        for str_id in str_ids:
+            id = int(str_id)
+            if id >= 0:
+                self.opt.gpu_ids.append(id)
+
+        # set gpu ids
+        if len(self.opt.gpu_ids) > 0:
+            torch.cuda.set_device(self.opt.gpu_ids[0])
+
+        args = vars(self.opt)
+
+        print('------------ Options -------------')
+        for k, v in sorted(args.items()):
+            print('%s: %s' % (str(k), str(v)))
+        print('-------------- End ----------------')
+
+        # save to the disk
+        self.mkdirs(self.opt.checkpoints_dir)
+        file_name = os.path.join(self.opt.checkpoints_dir, 'opt.txt')
+        with open(file_name, 'wt') as opt_file:
+            opt_file.write('------------ Options -------------\n')
+            for k, v in sorted(args.items()):
+                opt_file.write('%s: %s\n' % (str(k), str(v)))
+            opt_file.write('-------------- End ----------------\n')
+        return self.opt
+
+    def mkdir(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def mkdirs(self, paths):
+        if isinstance(paths, list) and not isinstance(paths, str):
+            for path in paths:
+                self.mkdir(path)
+        else:
+            self.mkdir(paths)
