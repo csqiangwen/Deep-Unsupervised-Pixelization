f37ebe3f8f56fe0a129861cdf86bf2739731bb3b
Qiang Wen <csqiangwen@gmail.com>
2020-02-25 10:31:04 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,1 @@
+python3 ./test.py --dataroot ./samples --no_dropout --resize_or_crop crop --gpu_ids 0 --how_many 1 --which_epoch 200
---
+++ train.sh
@@ -0,0 +1,1 @@
+python3 ./train.py --dataroot ./samples --resize_or_crop crop --gpu_ids 0
